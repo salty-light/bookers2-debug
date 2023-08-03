5cf9cd0c67075913b42bd377e1a4239576cb4689
Raichi Ando <135943914+salty-light@users.noreply.github.com>
2023-08-03 08:39:36 +0000

    5  kansei

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,16 @@
+class SearchesController < ApplicationController
+before_action :authenticate_user!
+
+  def search
+    @range = params[:range]
+    @word = params[:word]
+
+    if @range == "User"
+      @users = User.looks(params[:search], params[:word])
+      render "/searches/search_result"
+    else
+      @books = Book.looks(params[:search], params[:word])
+      render "/searches/search_result"
+    end
+  end
+end
